27772fcc65a0c575dad010481c023a653e2ccc49
Derek Hao Hu <phoenixinter@gmail.com>
2014-04-24 21:49:46 -0700

    Add SRM 618 950pts

---
+++ MovingRooksDiv1.cpp
@@ -0,0 +1,159 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class MovingRooksDiv1 {
+    public:
+    vector<int> move(vector<int> Y1, vector<int> Y2) {
+		vector<int> ret;
+		int n = Y1.size();
+
+		for (int i = 0; i < n; i++) {
+			if (Y1[i] < Y2[i]) return vector<int>{ -1 };
+			if (Y1[i] == Y2[i]) continue;
+			for (int j = i + 1; j < n; j++) {
+				if (Y1[i] < Y1[j]) continue;	//can not move by definition
+				if (Y1[j] < Y2[i]) continue;	//if moving like this then destination won't be reached
+				swap(Y1[i], Y1[j]);
+				if (ret.size() < 2500) {
+					ret.push_back(i);
+					ret.push_back(j);
+				}
+			}
+			if (Y1[i] != Y2[i]) return vector<int> {-1};
+		}
+
+		return ret;
+    }
+};
+
+// CUT begin
+ifstream data("MovingRooksDiv1.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(vector<int> Y1, vector<int> Y2, vector<int> __expected) {
+    time_t startClock = clock();
+    MovingRooksDiv1 *instance = new MovingRooksDiv1();
+    vector<int> __result = instance->move(Y1, Y2);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> Y1;
+        from_stream(Y1);
+        vector<int> Y2;
+        from_stream(Y2);
+        next_line();
+        vector<int> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(Y1, Y2, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1398400000;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 950 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "MovingRooksDiv1 (950 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
